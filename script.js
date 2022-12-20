3c96159f2e6c72d58e140fe447eecdf24eba8a2f
Narwhalssss <114067296+Narwhalssss@users.noreply.github.com>
2022-12-20 15:02:18 +0700

    update

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 var showdesc =  $(".lesson-icon")
 
 showdesc.mouseenter(function(){
-    'text-contents1'.fadeIn(1000)
+    'text-contents'.fadeIn(1000)
 })
 showdesc.mouseleave(function(){
-    'text-contents1'.fadeOut(1000)
+    'text-contents'.fadeOut(1000)
 })
 
